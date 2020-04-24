53b7f002e27ddd8b0e05f951c2d5561eff52108a
Sungyoun Kim <sg17990@gamil.com>
2020-04-24 12:38:47 +0900

    Second

---
+++ ArithmeticOperatorExample.java
@@ -0,0 +1,18 @@
+public class ArithmeticOperatorExample {
+	public static void main(String[] args) {
+	int num1 = 5, num2 = 2;
+	int result = num1 / num2; //2
+	System.out.println("result: " + result);
+	
+	result = num1 % num2;
+	System.out.println("result: " + result);
+	
+	int num3 = 123;
+	if (num3 % 2 == 0) {
+		System.out.println("Even");
+	} else {
+		System.out.println("Odd");
+	}
+	
+	}
+}	
---
+++ Hello.java
@@ -0,0 +1,18 @@
+package com.yedam.prod;  //
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Sungyoun Kim!");
+		int personHight = 10;
+		int personWeight = 20;
+		personHight = 50;
+		
+		String strMessage =  "Hello, world";
+		
+		
+		int result = personHight + personWeight;
+		System.out.println("personHight : " + personHight);
+		System.out.println(personHight + " + " + personWeight + " => result: " + result);
+		System.out.println(strMessage);
+	}
+}
