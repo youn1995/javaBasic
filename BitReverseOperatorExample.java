2ec6421c02a38a0b1ec4050273bc029abed8125d
Sungyoun Kim <sg17990@gamil.com>
2020-04-27 10:39:35 +0900

    add

--- BitReverseOperatorExample.java
+++ BitReverseOperatorExample.java
@@ -3,29 +3,26 @@ public class BitReverseOperatorExample {
 		int num1 = 10;
 		int num2 = ~num1;
 		int num3 = num2 + 1;
-		
 
-		//java2 conflict
+		// java2 conflict
 
-		
 		String returnValue1 = toBinaryString(num1);
 		System.out.println(returnValue1);
-		
+
 		String returnValue2 = toBinaryString(num2);
 		System.out.println(returnValue2);
-		
+
 		String returnValue3 = toBinaryString(num3);
 		System.out.println(returnValue3);
 	}
-	
+
 	public static String toBinaryString(int val) {
 		String str = Integer.toBinaryString(val);
-		while(str.length() < 32) {
+		while (str.length() < 32) {
 			str = "0" + str;
-			
+
 		}
 		return str;
-	
-	
+
 	}
 }
